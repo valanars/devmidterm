9d6a94e47e484d1a8738539aa9347ed60ad5417d
Valentine Morin <zm724@nyu.edu>
2018-03-05 22:27:35 -0500

    added examination mechanic, implemented ingredient lists, updated code to be a lot more clean (shoutout to my pals who taught me how to make stuff look better lmao)

---
+++ Assets/Scripts/RotateObj.cs
@@ -0,0 +1,25 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class RotateObj : MonoBehaviour
+{
+	public Transform killme;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Pickup()
+	{
+		transform.position = new Vector3(0, 5, 0);
+	}
+}
--- Assets/Scripts/charaScript.cs
+++ Assets/Scripts/charaScript.cs
@@ -1,80 +1,159 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class charaScript : MonoBehaviour
 {
-	public Text text;
-
 	private float timeLeft = 40.0f;
 	public Text timerText;
 	public Text scoreText;
 	private int score;
 	private int nuts;
-	
+
+	private bool zoom;
+
+	public Image peanutInfo;
+	public Image candyInfo;
+	public Image bbInfo;
+	public Image reesesInfo;
+	public Image cheerInfo;
+	public Image grassInfo;
+	public Image fishInfo;
+	public Image teethInfo;
+	public Image sbInfo;
+
+	private RaycastHit hit;
+
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-		text.enabled = false;
+		//set all the images to false
+		peanutInfo.enabled = false;
+		candyInfo.enabled = false;
+		bbInfo.enabled = false;
+		reesesInfo.enabled = false;
+		cheerInfo.enabled = false;
+		grassInfo.enabled = false;
+		fishInfo.enabled = false;
+		teethInfo.enabled = false;
+		sbInfo.enabled = false;
 
 		score = 0;
 		nuts = 0;
+
+		zoom = false;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		//timer go
 		timeLeft -= Time.deltaTime;
 		timerText.text = "Time Left:" + Mathf.Round(timeLeft);
-		
+
 		//game over if you haven't eaten anything good
 		if ((timeLeft < 0) & (nuts > 0))
 		{
 			SceneManager.LoadScene("GameOver");
 		}
-		
+
 		//game end if you have eaten some good boys
 		if ((timeLeft < 0) && (nuts < 1) && (score > 0))
 		{
 			SceneManager.LoadScene("GameEnd");
 		}
-		
+
 		//raycast for food
 		if (Input.GetMouseButtonDown(0))
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
 			if (Physics.Raycast(ray, out hit))
 			{
-				if (hit.transform.gameObject.tag == "Death") //decrease time left after eating a nut
-				{
-					timeLeft = timeLeft - 20.0f;
-					text.enabled = true;
-					nuts++;
-					text.text = "Uh oh! Looks like that had some nuts in it.";
-					Destroy(hit.transform.gameObject);
-				}
-				
-				if (hit.transform.gameObject.tag == "Safe") //if you eat not a nut, you increase score
-				{
-					timeLeft = timeLeft + 10.0f;
-	                score++;
-                    Destroy(hit.transform.gameObject);
-                 }	
-				
-				if (hit.transform.gameObject.tag == "Dont") //why would you eat an inedible object
-				{
-					text.enabled = true;
-					text.text = "A forbidden snack...";
-					score++;
-					timeLeft = timeLeft + 10.0f;
-					Destroy(hit.transform.gameObject);
-				}	
+				zoom = true;
 			}
 		}
-		
-		scoreText.text = "Snacks Eaten: " + score;
+
+	//get food info
+		if (zoom)
+		{
+			Image info = peanutInfo; //just set it to w/e bc it wont let me do shit otherwise lol
+			GameObject food = hit.transform.gameObject;
+			float timeChange = 0;
+			int nutChange = 0;
+			int scoreChange = 0;
+			
+			if (food.tag == "Peanut") //PEANUT
+			{
+				info = peanutInfo;
+				timeChange = -20f;
+				nutChange = 1;
+			} else if (food.tag == "Candy") //CANDY
+			{
+				info = candyInfo;
+				timeChange = -20f;
+				nutChange = 1;
+			} else if (food.tag == "BreadBad") //NAUGHTY LOAF
+			{
+				info = bbInfo;
+				timeChange = -20f;
+				nutChange = 1;
+			} else if (food.tag == "Reeses") //REESES
+			{
+				info = reesesInfo;
+				timeChange = -20f;
+				nutChange = 1;
+			} else if (food.tag == "Cheerios") //CHEERIOS
+			{
+				info = cheerInfo;
+				timeChange = -20f;
+				nutChange = 1;
+			} else if (food.tag == "Grass") //GRASS
+			{
+				info = grassInfo;
+				timeChange = -20f;
+				nutChange = 1;
+			} else if (food.tag == "Fish") //FISH
+			{
+				info = fishInfo;
+				timeChange = 0;
+				nutChange = 0;
+				scoreChange = 1;
+			} else if (food.tag == "Teeth") //TEETH
+			{
+				info = teethInfo;
+				timeChange = 0;
+				nutChange = 0;
+				scoreChange = 1;
+			} else if (food.tag == "BreadSafe") //BREAD
+			{
+				info = sbInfo;
+				timeChange = 0;
+				nutChange = 0;
+				scoreChange = 1;
+			}
+
+			info.enabled = true;
+			
+			if (Input.GetKeyDown(KeyCode.Backspace)) //escape view
+			{
+				info.enabled = false;
+				zoom = false;
+			}
+				
+			if (Input.GetKeyDown(KeyCode.E)) //eat obj
+			{
+				timeLeft = timeLeft + timeChange;
+				nuts = nuts + nutChange;
+				score = score + scoreChange;
+				Destroy(info);
+				Destroy(food);
+				zoom = false;
+			}
+			
+			scoreText.text = "Snacks Eaten: " + score;
+		}
 	}
 }
